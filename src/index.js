746ce366d0d27bbb863337b7e352d82b71b891b0
Ye Naing Tun <yenaingtun425@gmail.com>
2023-11-21 21:00:12 +0630

    updated save local

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const form = document.querySelector('#form');
 const todoInput = document.querySelector('#todo');
 const todoList = document.querySelector('.tasks');
 
+document.addEventListener("DOMContentLoaded", getTodos);
 form.addEventListener("submit", addTodo);
 
-function addTodo(e){
+function addTodo(e) {
     e.preventDefault();
     if (todoInput.value !== '') {
         const todoDiv = document.createElement("div");
@@ -42,15 +43,108 @@ function addTodo(e){
         trashButton.innerHTML = `delete`;
         trashButton.classList.add('trash-btn', 'bg-rose-200', 'px-2', 'py-1', 'rounded-lg', 'text-rose-600', 'hover:bg-rose-300', 'text-xs');
         btnDiv.appendChild(trashButton);
-        trashButton.addEventListener('click', () => {
+        trashButton.addEventListener('click', (e) => {
             if (confirm("Delete a todo!") == true) {
-                newTodo.parentNode.remove();
+                removeTodo(e);
             }
         });
         todoDiv.append(btnDiv);
         //attach final Todo
         todoList.appendChild(todoDiv);
+        saveLocalTodos(newTodo.innerHTML);
     } else {
         alert('u need to add todo');
     }
+}
+
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function removeTodo(e) {
+    const item = e.target;
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement.parentElement;
+        removeLocalTodos(todo);
+        todo.remove();
+    }
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+function getTodos() {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(function (todo) {
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add('todo', 'flex', 'justify-between', 'items-center', 'bg-yellow-200', 'text-gray-500', 'rounded-lg', 'px-3', 'py-1', 'mb-2');
+        const newTodo = document.createElement("li");
+        newTodo.classList.add("todo-item");
+        newTodo.innerText = todo;
+        todoDiv.appendChild(newTodo);
+        todoDiv.addEventListener('dblclick', (e) => {
+            const editTodo = prompt('edit todo', newTodo.innerHTML);
+            if (editTodo !== null && editTodo !== '') {
+                removeLocalTodos(e.target);
+                newTodo.innerHTML = editTodo;
+                saveLocalTodos(newTodo.innerHTML);
+            }
+        })
+        const btnDiv = document.createElement("div");
+        // complete button
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = `complete`;
+        completedButton.classList.add('complete-btn', 'bg-teal-200', 'px-2', 'py-1', 'mr-1', 'rounded-lg', 'text-teal-600', 'hover:bg-teal-300', 'text-xs');
+        btnDiv.appendChild(completedButton);
+        completedButton.addEventListener('click', (e) => {
+            if (newTodo.style.textDecoration == 'line-through') {
+                newTodo.style.textDecoration = 'none';
+                todoDiv.style.backgroundColor = 'yellow';
+                completedButton.innerHTML = 'complete';
+            } else {
+                newTodo.style.textDecoration = 'line-through';
+                todoDiv.style.backgroundColor = 'green';
+                completedButton.innerHTML = 'completed';
+            }
+        });
+        //trash button
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = `delete`;
+        trashButton.classList.add('trash-btn', 'bg-rose-200', 'px-2', 'py-1', 'rounded-lg', 'text-rose-600', 'hover:bg-rose-300', 'text-xs');
+        btnDiv.appendChild(trashButton);
+        trashButton.addEventListener('click', (e) => {
+            if (confirm("Delete a todo!") == true) {
+                removeTodo(e);
+            }
+        });
+        todoDiv.append(btnDiv);
+        //attach final Todo
+        todoList.appendChild(todoDiv);
+    });
 }
